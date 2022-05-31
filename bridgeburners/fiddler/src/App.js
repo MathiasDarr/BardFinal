88faa746e186a9e9788d1b2d435131bda38fe122
Mathias Darr <mddarr@gmail.com>
2022-05-31 01:28:27 -0700

    add stuff hopefully not node modules

---
+++ bridgeburners/fiddler/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Silly from './silly'
+function App() {
+  return (
+    <div className="App">
+      DFdfadfa dfdf
+      <Silly />
+    </div>
+  );
+}
+
+export default App;
---
+++ bridgeburners/fiddler/src/silly.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Silly extends React.Component {
+  render(){
+    return(<div>BardfdScreen</div>)
+  }
+}
+export default Silly;
